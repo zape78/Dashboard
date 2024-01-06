421cdfdd5676aae7ae684a3d160d87ce87a64e8e
Brian Zapien <brianzapien@gmail.com>
2024-01-05 17:14:57 -0800

    Created Contacts Scene, updated App

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -69,7 +69,7 @@ const Team = () => {
 
     return (
         <Box m="20px">
-            <Header />
+            <Header title="MANAGE TEAM" subtitle="Managing Team Members"/>
             <Box
             m="40px 0 0 0" 
             height="75vh" 
