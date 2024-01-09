71f4c93bc8971fad4548c110672655d72d57466a
Brian Zapien <brianzapien@gmail.com>
2024-01-08 23:29:06 -0800

    Added Grid Row 2 for DashBoard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -78,7 +78,7 @@ const Dashboard = () => {
                     justifyContent="center"
                 >
                     <StatBox
-                        title="431,225"
+                        title="$43,225"
                         subtitle="Sales Obtained"
                         progress="0.5"
                         increase="+21%"
@@ -139,6 +139,118 @@ const Dashboard = () => {
                     />
                 </Box>
 
+                {/* ROW 2 LINE CHART*/}
+                <Box
+                    gridColumn="span 8" 
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]} 
+                >
+                    <Box
+                        mt="25px" 
+                        p="0 30px" 
+                        display="flex"
+                        justifyContent="space-between" 
+                        alignItems="center"
+                    >
+                        <Box>
+                            <Typography 
+                                variant="h5" 
+                                fontWeight="600" 
+                                color={colors.grey[100]}
+                            >
+                                Revenue Generated 
+                            </Typography>
+                            <Typography 
+                                variant="h3" 
+                                fontWeight="500" 
+                                color={colors.greenAccent[500]}
+                            >
+                                $59,342.32 
+                            </Typography>
+                        </Box>
+
+                        <Box>
+                            <IconButton>
+                                <DownloadOutlinedIcon
+                                    sx={{ 
+                                        fontSize: "26px", 
+                                        color: colors.greenAccent[500]
+                                    }}
+                                />
+                            </IconButton>
+                        </Box>
+                    </Box>
+
+                    <Box 
+                        height="250px" 
+                        ml="-20px"
+                    >
+                        <LineChart isDashboard={true} />
+                    </Box>
+                </Box>    
+
+                    {/* ROW 2 TRANSACTIONS */}
+                <Box
+                    gridColumn="span 4" 
+                    gridRow="span 2"
+                    backgroundColor={colors.primary[400]} 
+                    overflow="auto" 
+                >
+
+                    <Box
+                        display="flex" 
+                        justifyContent="space-between" 
+                        alignItems="center" 
+                        borderBottom={`4px solid ${colors.primary[500]}`} 
+                        colors={colors.grey[100]}
+                        p="15px" 
+                    >
+                        <Typography
+                            color={colors.grey[100]} 
+                            variant="h5" 
+                            fontweight="600"
+                        >
+                            Recent Transactions
+                        </Typography>
+                    </Box>
+                    {mockTransactions.map((transaction, i) => (
+                        <Box
+                            key={`${transaction.txId}-${i}`} 
+                            display="flex" 
+                            justifyContent="space-between" 
+                            alignContent="center"
+                            borderBottom={`4px solid ${colors.primary[500]}`} 
+                            p="15px" 
+                        >
+                            <Box>
+                                <Typography
+                                    color={colors.greenAccent[500]} 
+                                    variant="h5" 
+                                    fontweight="600"
+                            >
+                                {transaction.txId}
+                                </Typography>
+                                <Typography
+                                    color={colors.grey[100]}
+                                >
+                                    {transaction.user}
+                                </Typography>
+                            </Box>
+                            <Box 
+                                color={colors.grey[100]}
+                            >
+                                {transaction.date}
+                            </Box>
+                            <Box
+                                backgroundColor={colors.greenAccent[500]} 
+                                p="5px 10px" 
+                                borderRadius="4px"
+                            >
+                                ${transaction.cost}
+                            </Box>
+                        </Box>
+                    ))}
+                </Box>
             </Box>
         </Box>
     );
